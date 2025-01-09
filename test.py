09fd9d4752587157551bc9b49d3a561a86645f87
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-09 10:23:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -91,7 +91,8 @@ if submit:
                        if st.download_button(label=f"Download {file}", data=open(f"{dir_name}/{file}", "rb"), file_name=file):
                            st.success("✅ Video Downloaded Successfully!")
                    shutil.rmtree(dir_name)
-   except:
+   except Exception as e:
+       st.error(e)
        st.error("OOPS! Enter a valid URL....")
 
 
